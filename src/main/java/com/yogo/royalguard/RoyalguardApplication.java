ee47c073b55413f8f8342921e654fdcf6f4b3549
yogo254 <yogo254@yahoo.com>
2020-07-02 11:56:54 +0300

    initial commit

---
+++ src/main/java/com/yogo/royalguard/RoyalguardApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.royalguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalguardApplication.class, args);
+	}
+
+}
